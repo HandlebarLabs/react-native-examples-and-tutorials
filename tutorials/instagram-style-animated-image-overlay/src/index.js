a07527bb48bffaf33dc82ac1850f2747e2eb3302
Spencer Carli <spencercarli@gmail.com>
2018-09-05 13:14:53 -0700

    Add animated overlay image logic

--- tutorials/instagram-style-animated-image-overlay/src/index.js
+++ tutorials/instagram-style-animated-image-overlay/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity, Animated } from 'react-native';
 
 import DoubleTap from './DoubleTap';
 
@@ -9,18 +9,59 @@ export default class App extends React.Component {
   state = {
     liked: false,
   };
+  animatedValue = new Animated.Value(0);
 
-  toggleLike = () => this.setState(state => ({ liked: !state.liked }));
+  toggleLike = () => this.setState(state => {
+    const newLiked = !state.liked;
+
+    if (newLiked) {
+      Animated.sequence([
+        Animated.spring(this.animatedValue, { toValue: 1 }),
+        Animated.spring(this.animatedValue, { toValue: 0 }),
+      ]).start();
+    }
+
+    return { liked: newLiked };
+  });
+
+  renderOverlay = () => {
+    const imageStyles = [
+      styles.overlayHeart,
+      {
+        opacity: this.animatedValue,
+        transform: [
+          {
+            scale: this.animatedValue.interpolate({
+              inputRange: [0, 1],
+              outputRange: [0.7, 1.5]
+            }),
+          }
+        ]
+      },
+    ];
+
+    return (
+      <View style={styles.overlay}>
+        <Animated.Image
+          source={require('./images/heart.png')}
+          style={imageStyles}
+        />
+      </View>
+    );
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <DoubleTap onDoubleTap={this.toggleLike}>
-          <Image
-            source={{ uri: `https://images.pexels.com/photos/671557/pexels-photo-671557.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=${w.width}` }}
-            style={{ width: w.width, height: w.width }}
-            resizeMode="cover"
-          />
+          <View>
+            <Image
+              source={{ uri: `https://images.pexels.com/photos/671557/pexels-photo-671557.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=${w.width}` }}
+              style={{ width: w.width, height: w.width }}
+              resizeMode="cover"
+            />
+            {this.renderOverlay()}
+          </View>
         </DoubleTap>
         <View style={styles.iconRow}>
           <TouchableOpacity onPress={this.toggleLike}>
@@ -53,5 +94,17 @@ const styles = StyleSheet.create({
   heartIcon: {
     width: 20,
     height: 20,
+  },
+  overlay: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+  overlayHeart: {
+    tintColor: '#fff'
   },
 });
