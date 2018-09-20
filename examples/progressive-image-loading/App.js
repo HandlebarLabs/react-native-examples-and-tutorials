67a565a56d62b79dea6f644aaba5a472ccddaed2
Spencer Carli <spencercarli@gmail.com>
2018-09-20 15:41:56 -0700

    Add a progressive image loading example (#5)
    * Add a progressive image loading example
    
    * Fix linting

---
+++ examples/progressive-image-loading/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import ProgressiveImage from './ProgressiveImage';
+
+const w = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ProgressiveImage
+          thumbnailSource={{ uri: `https://images.pexels.com/photos/671557/pexels-photo-671557.jpeg?w=10&buster=${Math.random()}` }}
+          source={{ uri: `https://images.pexels.com/photos/671557/pexels-photo-671557.jpeg?w=${w.width * 2}&buster=${Math.random()}` }}
+          style={{ width: w.width, height: w.width }}
+          resizeMode="cover"
+        />
+      </View>
+    );
+  }
+}
