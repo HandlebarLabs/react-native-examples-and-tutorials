b0743b081f5510f8e28d6119206fa4df322abed6
Spencer Carli <spencercarli@gmail.com>
2018-09-25 16:50:39 -0500

    Add a rotating image example

---
+++ examples/rotating-local-images/src/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bannerImage: {
+    width: '100%',
+  },
+});
+
+const BANNER_ADS = [
+  {
+    name: 'Cat',
+    path: require('./images/1.jpg'),
+  },
+  {
+    name: 'Plant',
+    path: require('./images/2.jpg'),
+  },
+  {
+    name: 'Dog',
+    path: require('./images/3.jpg'),
+  },
+];
+
+const getRandomIndex = () => Math.floor(
+  Math.random() * BANNER_ADS.length,
+);
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentImageIndex: getRandomIndex(),
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.displayRandomBannerAd();
+      console.log('changed ad');
+    }, 4000);
+  }
+
+  displayRandomBannerAd = () => {
+    this.setState({
+      currentImageIndex: getRandomIndex(),
+    });
+  };
+
+  render() {
+    if (BANNER_ADS.length > 0) {
+      return (
+        <View style={styles.container}>
+          <Image
+            style={styles.bannerImage}
+            source={BANNER_ADS[this.state.currentImageIndex].path}
+            resizeMode="contain"
+          />
+          <Text>{BANNER_ADS[this.state.currentImageIndex].name}</Text>
+        </View>
+      );
+    }
+
+    return null;
+  }
+}
