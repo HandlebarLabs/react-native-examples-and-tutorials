742e0684a5a51d5547cf716103d52140566a3c2f
Spencer Carli <spencercarli@gmail.com>
2018-10-12 11:45:18 -0500

    Multi tap and long press

--- tutorials/detect-multiple-taps/App.js
+++ tutorials/detect-multiple-taps/App.js
@@ -1,15 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import { StyleSheet, View, TouchableHighlight } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +8,62 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    backgroundColor: '#cc0000',
+    width: 200,
+    height: 200,
+    borderRadius: 5,
+  },
 });
+
+class MultiTap extends React.Component {
+  static defaultProps = {
+    onPress: () => null,
+    numberOfTouches: 2,
+    delay: 1000,
+  }
+
+  startPress = null;
+
+  onStartShouldSetResponder = (evt) => {
+    if (evt.nativeEvent.touches.length === this.props.numberOfTouches) {
+      this.startPress = Date.now();
+      return true;
+    }
+
+    return false;
+  }
+
+  onResponderRelease = () => {
+    const now = Date.now();
+    if (this.startPress && now - this.startPress > this.props.delay) {
+      this.props.onPress();
+    }
+    this.startPress = null;
+  }
+
+  render() {
+    return (
+      <View
+        onStartShouldSetResponder={this.onStartShouldSetResponder}
+        onResponderRelease={this.onResponderRelease}
+      >
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MultiTap onPress={() => alert('double tap!')}>
+          <TouchableHighlight onPress={() => alert('box tapped!')}>
+            <View style={styles.box} />
+          </TouchableHighlight>
+        </MultiTap>
+      </View>
+    );
+  }
+}
