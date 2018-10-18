cb7de32e2dc21e5a8c18c5894763454ca7b7fab6
Spencer Carli <spencercarli@gmail.com>
2018-10-18 17:17:24 -0500

    Trying to get started - gesture handler bug blocking though

---
+++ examples/swipe-hint/AppleStyleSwipeableRow.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { GestureHandler } from 'expo';
+
+const { Swipeable, RectButton } = GestureHandler;
+
+const styles = StyleSheet.create({
+  leftAction: {
+    flex: 1,
+    backgroundColor: '#497AFC',
+    justifyContent: 'center',
+  },
+  actionText: {
+    color: 'white',
+    fontSize: 16,
+    backgroundColor: 'transparent',
+    padding: 10,
+  },
+  rightAction: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+export default class AppleStyleSwipeableRow extends Component {
+  renderLeftActions = (progress, dragX) => {
+    const trans = dragX.interpolate({
+      inputRange: [0, 50, 100, 101],
+      outputRange: [-20, 0, 0, 1],
+    });
+    return (
+      <RectButton style={styles.leftAction} onPress={this.close}>
+        <Animated.Text
+          style={[
+            styles.actionText,
+            {
+              transform: [{ translateX: trans }],
+            },
+          ]}
+        >
+          Archive
+        </Animated.Text>
+      </RectButton>
+    );
+  };
+
+  renderRightAction = (text, color, x, progress) => {
+    // const trans = progress.interpolate({
+    //   inputRange: [0, 1],
+    //   outputRange: [x, 0],
+    // });
+    const pressHandler = () => {
+      this.close();
+      // alert(text);
+    };
+    const s = {
+      flex: 1,
+      // transform: [{ translateX: trans }]
+    };
+    return (
+      <Animated.View style={s}>
+        <RectButton
+          style={[styles.rightAction, { backgroundColor: color }]}
+          onPress={pressHandler}
+        >
+          <Text style={styles.actionText}>{text}</Text>
+        </RectButton>
+      </Animated.View>
+    );
+  };
+
+  renderRightActions = (progress, dragX) => {
+
+    return (
+      <View style={{ width: 192, flexDirection: 'row' }}>
+        {/* {this.renderRightAction('More', '#C8C7CD', 192, progress)}
+        {this.renderRightAction('Flag', '#ffab00', 128, progress)} */}
+        {this.renderRightAction('More', '#dd2c00', 64, progress)}
+      </View>
+    );
+  }
+
+  updateRef = (ref) => {
+    this._swipeableRow = ref;
+  };
+
+  close = () => {
+    this._swipeableRow.close();
+  };
+
+  render() {
+    const { children } = this.props;
+    return (
+      <Swipeable
+        ref={this.updateRef}
+        friction={2}
+        leftThreshold={30}
+        rightThreshold={40}
+        renderLeftActions={this.renderLeftActions}
+        renderRightActions={this.renderRightActions}
+      >
+        {children}
+      </Swipeable>
+    );
+  }
+}
