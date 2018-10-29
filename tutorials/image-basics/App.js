01608e81e48f672be653816f831751be809808ee
Spencer Carli <spencercarli@gmail.com>
2018-10-29 11:32:15 -0500

    fix linting problems

--- tutorials/image-basics/App.js
+++ tutorials/image-basics/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   StyleSheet,
-  Text,
   View,
   Image,
 } from 'react-native';
@@ -29,5 +28,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-
